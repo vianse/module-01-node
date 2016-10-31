7a37be28d66d62bb2407909eb17c8fd99e01ca32
Juan Carlos Santiago <krlos013@gmail.com>
2016-10-30 19:16:15 -0600

    modificacion en el paquete

--- index.js
+++ index.js
@@ -23,5 +23,8 @@ io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
+    io.on('disconnect', function (socket) {
+    	console.log(data);
+    });
   });
 });
