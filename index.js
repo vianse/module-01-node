93313e7691d2dcbc8ca7c9586754e829f3314519
Juan Carlos Santiago <krlos013@gmail.com>
2016-10-30 18:45:08 -0600

    modificacion en el paquete

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
 
 var port = process.env.PORT || 8080;
 
+app.listen(port);
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
   });
 });
-app.listen(port, function () {
-
-    console.log('Facebook Messenger Bot on port: ' + port);
-
-});
-
