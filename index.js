cf9aca69067daf101f20215470b310570f5b2420
Juan Carlos Santiago <krlos013@gmail.com>
2016-10-30 19:50:35 -0600

    modificacion en el paquete

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ io.on('connection', function (socket) {
   });
 });
 
-socket.emit("datos", "Consulta exitosa desde el server");
+oi.emit("datos", "Consulta exitosa desde el server");
 // get is a simple wrapper for request()
 // which sets the http method to GET
 var request = http.get(url, function (response) {
