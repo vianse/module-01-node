49253d24ccaf05f8715b09131bf9c7c989c19a45
Juan Carlos Santiago <krlos013@gmail.com>
2016-11-01 23:00:23 -0600

    modificacion en el paquete

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
- var app = require('http').createServer(handler)
-// var io = require('socket.io')(app);
-// var fs = require('fs');
- var port = process.env.PORT || 8080;
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
 
-// app.listen(port);
+app.listen(8080);
 
-// note, io(<port>) will create a http server for you
-var io = require('socket.io')(port);
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
 
 io.on('connection', function (socket) {
-  io.emit('this', { will: 'be received by everyone'});
-
-  socket.on('private message', function (from, msg) {
-    console.log('I received a private message by ', from, ' saying ', msg);
-  });
-
-  socket.on('disconnect', function () {
-    io.emit('user disconnected');
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
   });
 });
