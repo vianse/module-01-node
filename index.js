0b068d37476e90dfb1b2b8e30033081518baab3b
Juan Carlos Santiago <krlos013@gmail.com>
2016-11-01 21:04:16 -0600

    modificacion en el paquete

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var app = require('http').createServer(handler)
-var http = require("http");
+
 var io = require('socket.io')(app);
 var fs = require('fs');
-url = "http://maps.googleapis.com/maps/api/directions/json?origin=Central Park&destination=Empire State Building&sensor=false&mode=walking";
 var port = process.env.PORT || 8080;
 
 app.listen(port);
@@ -38,31 +37,3 @@ io.on('connection', function (socket) {
   socket.emit('datos', "Datos desde el servidor");
   
 });
-
-// get is a simple wrapper for request()
-// which sets the http method to GET
-var request = http.get(url, function (response) {
-    // data is streamed in chunks from the server
-    // so we have to handle the "data" event    
-    var buffer = "", 
-        data,
-        route;
-
-    response.on("data", function (chunk) {
-        buffer += chunk;
-    }); 
-
-    response.on("end", function (err) {
-        // finished transferring data
-        // dump the raw data
-        console.log(buffer);
-        console.log("\n");
-        data = JSON.parse(buffer);
-        route = data.routes[0];
-        console.log("Walking Distance: " + route.legs[0].distance.text);
-        console.log("Time: " + route.legs[0].duration.text);
-        // extract the distance and time
-
-        
-    }); 
-}); 
