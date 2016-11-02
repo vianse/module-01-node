a01c37352a65786d433e65e90c12ce31839838f6
Juan Carlos Santiago <krlos013@gmail.com>
2016-11-01 21:57:58 -0600

    modificacion en el paquete

--- index.js
+++ index.js
@@ -17,5 +17,9 @@ function handler (req, res) {
   });
 }
 
- io.emit('resultado', { hello: 'world' });
-
+ io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
